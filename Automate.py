ec8b2fed12f9ec18dbe7f38a32fb651b464d180b
abhinav1829 <35770758+abhinav1829@users.noreply.github.com>
2019-11-09 00:37:12 +0530

    Update Automate.py
    Login and Register functions are added

--- Automate.py
+++ Automate.py
@@ -1,91 +1,109 @@
-from tkinter import *
-from tkinter import Text
-
-from automate import listen
 import datetime
 import os
 import re
 import urllib
 import webbrowser
+from tkinter import *
+from tkinter import Text
+
 import speech_recognition as sr
 import wikipedia
 from win32com.client import Dispatch
 
 tts = Dispatch("SAPI.SpVoice")
-root = Tk()
+login_root = Tk()
+flag = 0
 
 
-def initialize():
-    root.title('Automate')
-    root.state('zoomed')
+def check_credentials(username_entry, password_entry):
+    file = open('users', 'r')
+    entries = file.readlines()
+    file.close()
 
-    Grid.rowconfigure(root, 0, weight=1)
-    Grid.columnconfigure(root, 0, weight=1)
-    f = Frame(root)
-    f.grid(row=0, column=0, sticky=N + S + E + W)
-
-    Grid.rowconfigure(f, 1, weight=8)
-    Grid.columnconfigure(f, 0, weight=1)
-
-    f_settings = Frame(f)
-    f_settings.grid(row=1, column=0, sticky=N + S + E + W)
-
-    f_about = Frame(f)
-    f_about.grid(row=1, column=0, sticky=N + S + E + W)
-
-    f_automate = Frame(f)
-    f_automate.grid(row=1, column=0, sticky=N + S + E + W)
-
-    initf_automate(f_automate)
-    initf_settings(f_settings)
-    initf_about(f_about)
-
-    Grid.rowconfigure(f, 0, weight=1)
-    Grid.columnconfigure(f, 0, weight=1)
-    f_menu = Frame(f)
-    f_menu.grid(row=0, column=0, sticky=N + S + E + W)
-    initf_menu(f_menu,f_automate,f_settings,f_about)
-
+    global flag
+    for entry in entries:
+        if username_entry == entry.split(' ')[0]:
+            flag = 1
+            if password_entry == entry.split(' ')[1].split('\n')[0]:
+                flag = 2
+                login_root.destroy()
+                return
+    if flag == 0:
+        print('Invalid username')
+    elif flag == 1:
+        print('Invalid password')
+        flag = 0
     pass
 
 
-def initf_menu(f_menu, f_automate,f_settings,f_about):
-    for row_index in range(1):
-        Grid.rowconfigure(f_menu, row_index, weight=1)
-        for col_index in range(3):
-            Grid.columnconfigure(f_menu, col_index, weight=1)
+def new_entry(username_entry, password_entry):
+    file = open('users', 'r')
+    entries = file.readlines()
+    file.close()
+    for entry in entries:
+        if username_entry == entry.split(' ')[0]:
+            print('User already exists')
+            return
 
-    btn1 = Button(f_menu, text='Automate',command=f_automate.lift)
-    btn1.grid(row=0, column=0, sticky=N + S + E + W)
-
-    btn2 = Button(f_menu, text='Settings',command=f_settings.lift)
-    btn2.grid(row=0, column=1, sticky=N + S + E + W)
-
-    btn3 = Button(f_menu, text='About',command=f_about.lift)
-    btn3.grid(row=0, column=2, sticky=N + S + E + W)
+    file = open('users', 'a')
+    file.write(username_entry + " " + password_entry + "\n")
+    file.close()
+    print('Account created')
     pass
 
 
-def initf_automate(f_automate):
-    Grid.rowconfigure(f_automate, 0, weight=1)
-    Grid.columnconfigure(f_automate, 1, weight=1)
-    txt = Text(f_automate)
-    txt.grid(row=0, column=1, sticky=N + S + E + W)
+def del_entry(username_entry, password_entry):
+    file = open('users', 'r')
+    entries = file.readlines()
+    file.close()
+    flag1 = True
+    for entry in entries:
+        if username_entry == entry.split(' ')[0] and password_entry == entry.split(' ')[1].split('\n')[0]:
+            entries.remove(entry)
+            print('Account deleted')
+            flag1 = False
+            break
+    if flag1:
+        print('No such account')
+        return
 
-    Grid.rowconfigure(f_automate, 0, weight=1)
-    Grid.columnconfigure(f_automate, 0, weight=1)
-    btn4 = Button(f_automate, text='Listen', command=lambda: listen(txt))
-    btn4.grid(row=0, column=0, sticky=N + S + E + W)
-
+    file = open('users', 'w')
+    for entry in entries:
+        file.write(entry)
+    file.close()
     pass
 
 
-def initf_settings(f_settings):
+def login_initialize():
+    login_root.title('Login')
+    username_label = Label(login_root, text='Username : ')
+    username_label.grid(row=0, column=0, sticky=N + S + E + W)
+    password_label = Label(login_root, text='Password : ')
+    password_label.grid(row=1, column=0, sticky=N + S + E + W)
+
+    username_entry = Entry(login_root)
+    username_entry.grid(row=0, column=1, sticky=N + S + E + W)
+    password_entry = Entry(login_root, show='*')
+    password_entry.grid(row=1, column=1, sticky=N + S + E + W)
+
+    del_user_btn = Button(login_root, text='Del User',
+                          command=lambda: del_entry(username_entry.get(), password_entry.get()))
+    del_user_btn.grid(row=2, column=0, sticky=N + S + E + W)
+    signup_btn = Button(login_root, text='Sign Up',
+                        command=lambda: new_entry(username_entry.get(), password_entry.get()))
+    signup_btn.grid(row=2, column=1, sticky=N + S + E + W)
+    login_btn = Button(login_root, text='Login',
+                       command=lambda: check_credentials(username_entry.get(), password_entry.get()))
+    login_btn.grid(row=3, column=0, columnspan=2, sticky=N + S + E + W)
+    login_root.mainloop()
     pass
 
 
-def initf_about(f_about):
+login_initialize()
+if flag == 2:
     pass
+else:
+    exit()
 
 
 def speak(query, txt):
@@ -94,6 +112,7 @@ def speak(query, txt):
     txt.insert(INSERT, '\n')
     txt.update()
     tts.Speak(query)
+    pass
 
 
 def wish_me(txt):
@@ -105,6 +124,7 @@ def wish_me(txt):
     else:
         speak("Good evening", txt)
     speak("I am automate . How may I help you ?", txt)
+    pass
 
 
 def take_command(txt):
@@ -140,10 +160,10 @@ def listen(txt):
         results = re.findall('href=\"\\/watch\\?v=(.{11})', htm_content.read().decode())
         webbrowser.open("https://www.youtube.com/watch?v=" + results[0])
     elif 'wikipedia' in query:
-        speak('Searching Wikipedia', txt)
+        speak('searching wikipedia', txt)
         query = query.replace("wikipedia", "").strip()
         results = wikipedia.summary(query, sentences=2)
-        speak('According to wikipedia', txt)
+        speak('according to wikipedia', txt)
         print(results)
         speak(results, txt)
     elif 'google' in query:
@@ -151,15 +171,86 @@ def listen(txt):
         query = query.replace("google", "").strip()
         webbrowser.open('https://www.google.com/search?q=' + query)
     elif 'play movie' in query:
-        speak('Playing movie Active Measures .', txt)
+        speak('playing movie active measures .', txt)
         movie_dir = 'G:\Movies\Active Measures'
         movies = os.listdir(movie_dir)
         os.startfile(os.path.join(movie_dir, movies[0]))
     elif 'bye' in query:
         speak('Goodbye', txt)
         exit()
+    else:
+        speak('I am not sure about this')
 
 
-if __name__ == '__main__':
-    initialize()
+def initf_automate(f_automate):
+    Grid.rowconfigure(f_automate, 0, weight=1)
+    Grid.columnconfigure(f_automate, 1, weight=1)
+    txt = Text(f_automate)
+    txt.grid(row=0, column=1, sticky=N + S + E + W)
+
+    Grid.rowconfigure(f_automate, 0, weight=1)
+    Grid.columnconfigure(f_automate, 0, weight=1)
+    btn4 = Button(f_automate, text='Listen', command=lambda: listen(txt))
+    btn4.grid(row=0, column=0, sticky=N + S + E + W)
+
+    pass
+
+
+def initf_settings(f_settings):
+    pass
+
+
+def initf_about(f_about):
+    pass
+
+
+def initf_menu(f_menu, f_automate, f_settings, f_about):
+    for row_index in range(1):
+        Grid.rowconfigure(f_menu, row_index, weight=1)
+        for col_index in range(3):
+            Grid.columnconfigure(f_menu, col_index, weight=1)
+
+    btn1 = Button(f_menu, text='Automate', command=f_automate.lift)
+    btn1.grid(row=0, column=0, sticky=N + S + E + W)
+
+    btn2 = Button(f_menu, text='Settings', command=f_settings.lift)
+    btn2.grid(row=0, column=1, sticky=N + S + E + W)
+
+    btn3 = Button(f_menu, text='About', command=f_about.lift)
+    btn3.grid(row=0, column=2, sticky=N + S + E + W)
+    pass
+
+
+def initialize():
+    root = Tk()
+    root.title('Automate')
+    root.state('zoomed')
+
+    Grid.rowconfigure(root, 0, weight=1)
+    Grid.columnconfigure(root, 0, weight=1)
+    f = Frame(root)
+    f.grid(row=0, column=0, sticky=N + S + E + W)
+
+    Grid.rowconfigure(f, 1, weight=8)
+    Grid.columnconfigure(f, 0, weight=1)
+    f_settings = Frame(f)
+    f_settings.grid(row=1, column=0, sticky=N + S + E + W)
+    f_about = Frame(f)
+    f_about.grid(row=1, column=0, sticky=N + S + E + W)
+    f_automate = Frame(f)
+    f_automate.grid(row=1, column=0, sticky=N + S + E + W)
+
+    initf_automate(f_automate)
+    initf_settings(f_settings)
+    initf_about(f_about)
+
+    Grid.rowconfigure(f, 0, weight=1)
+    Grid.columnconfigure(f, 0, weight=1)
+    f_menu = Frame(f)
+    f_menu.grid(row=0, column=0, sticky=N + S + E + W)
+    initf_menu(f_menu, f_automate, f_settings, f_about)
     root.mainloop()
+    pass
+
+
+initialize()
